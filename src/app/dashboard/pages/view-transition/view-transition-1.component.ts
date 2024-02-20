88186e284a2c6c1a197f16018ca7978d8b4d19a2
NixonAmado <nixonamado00@gmail.com>
2024-02-20 14:30:33 -0500

    feat: :zap: Transitions
    has been added view transition 2 for implement Hero transition of the same object in different pages

---
+++ src/app/dashboard/pages/view-transition/view-transition-1.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule,TitleComponent],
+  template: `
+    <app-title title="View Transition 1"/>
+    <section class="flex justify-start ">    
+      <div
+        class=" bg-blue-500 w-full relative z-10 h-dvh"
+        style="view-transition-name:hero2"
+        >
+        <img srcset="https:picsum.photos/id/237/200/300"
+      alt="Picsum"
+      width="200"
+      height="300"
+      class= "border-spacing-20 border-separate top-44 relative p-5 bg-black"
+      style="view-transition-name:hero1; z-index:-1; background:red;"
+
+      />
+
+
+      </div>
+    </section>
+  `
+})
+export default class ViewTransitionComponent {
+
+}
