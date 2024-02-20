7e80d43a22194867e970c828e24a39436553140a
NixonAmado <nixonamado00@gmail.com>
2024-02-20 14:35:44 -0500

    feat: :sparkles: Implementing Heavy loaders fast componnet
    fast loader component creation, it has and @Imput parameter and <ng.content> tag for flexibilitiy

--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <h1>Hola mundo</h1>
-  `
+  <section [ngClass]= "['w-full', cssClass]">
+    <ng-content />
+  </section>
+`
 })
 export class HeavyLoadersFastComponent {
-
+  @Input( { required: true}) cssClass!: string
 }
+
+// El elemento <ng-content> sirve para proyectar contenido dentro de una plantilla de componente en Angular1. Esto significa que puedes insertar elementos HTML o componentes personalizados dentro de otro componente, y controlar cómo se renderizan y estilan2.
