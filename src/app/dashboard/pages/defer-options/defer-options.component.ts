a36b01d7819728c901904b9c00350c6ecafd8cfb
NixonAmado <nixonamado00@gmail.com>
2024-02-20 14:32:09 -0500

    feat: :sparkles: Trying defer Options
    Playing with @defer and its possible parameters

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersFastComponent, TitleComponent, CommonModule],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
 export default class DeferOptionsComponent {
-
+  constructor(){
+    console.log("HeavyLoader Fast Creadoo")
+  }
 }
